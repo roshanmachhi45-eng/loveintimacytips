09248c22880d5f3c5b3690721b81ce3f84fa5f72
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-08-17 22:01:34 +0530

    Delete src/lib/sanity.ts

--- src/lib/sanity.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@sanity/client'
-
-export const sanityClient = createClient({
-  projectId: 'xdcfohh8',
-  dataset: 'production',
-  apiVersion: '2026-08-16',
-  useCdn: true,
-})
