01f675fad70f0ee913bae4aea56635e0ab059d3f
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-05 12:17:23 +0530

    Add contact email to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@
 import { Shield, Mail } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Logo from './Logo';
-contact.loveons@gmail.com
+
+
 const footerLinks = [
   { label: 'About Us', to: '/about' },
   { label: 'Contact Us', to: '/contact' },
