54ee8e6ca78f4cf366f015c92c07bb793b139c32
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-07 20:21:05 +0530

    Update Firebase configuration values
    Updated Firebase configuration values for apiKey, messagingSenderId, and appId.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,12 +11,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBJfn2Ive_yfleJeEabVfaqo8EN_JfIj5g',
+  apiKey: 'AIzaSyBJfn2ive_YfleJeEabVfaqo8EN_JfIj5g',
   authDomain: 'loveons.firebaseapp.com',
   projectId: 'loveons',
   storageBucket: 'loveons.firebasestorage.app',
-  messagingSenderId: '791675098600',
-  appId: '1:791675098600:web:1209bda0c838172f81b49d',
+  messagingSenderId: '791675090600',
+  appId: '1:791675090600:web:1209bda0c838172f01b49d',
   measurementId: 'G-DJSXYEN7TY',
 };
 
