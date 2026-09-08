a19633560564d0f60abfe39696bf067d7af59a28
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-09 01:23:39 +0530

    Update Firebase configuration and initialization

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,27 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJfn2Ive_YfIeJeEabVfaq8EN_JfIj5g",
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJfn2Ive_YfleJeEabVfaqo8EN_JfIj5g",
   authDomain: "loveons.firebaseapp.com",
   projectId: "loveons",
   storageBucket: "loveons.firebasestorage.app",
   messagingSenderId: "791675090600",
   appId: "1:791675090600:web:1209bda0c838172f01b49d",
-  measurementId: "G-DJSXYEN7TY",
+  measurementId: "G-DJSXYEN7TY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); 
 };
 
 const app = initializeApp(firebaseConfig);
