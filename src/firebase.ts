3009e4eb33ab6994230b3e8fb5576bfa5425643e
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-08 20:21:06 +0530

    Add Firebase initialization and authentication setup
    Initialize Firebase app with configuration and set up authentication.

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJfn2Ive_YfIeJeEabVfaq8EN_JfIj5g",
+  authDomain: "loveons.firebaseapp.com",
+  projectId: "loveons",
+  storageBucket: "loveons.firebasestorage.app",
+  messagingSenderId: "791675090600",
+  appId: "1:791675090600:web:1209bda0c838172f01b49d",
+  measurementId: "G-DJSXYEN7TY",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const googleProvider = new GoogleAuthProvider();
+
+export default app;
