5f1f8fbc5aeff3c55bb7dbd83fe49cc410239e4d
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-10 20:47:03 +0530

    Refactor hero text div for background styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -417,9 +417,10 @@ export default function Home() {
             <div className="grid items-stretch lg:grid-cols-2">
 
               {/* HERO TEXT */}
+               <div className="flex flex-col justify-center px-6 py-10 sm:px-10 sm:py-14 lg:px-14 lg:py-16 xl:px-16 bg-[url('/images/heart-bg.png')] bg-no-repeat bg-left-center bg-contain lg:bg-[length:70%]">
 
-              <div className="flex flex-col justify-center px-6 py-10 sm:px-10 sm:py-14 lg:px-14 lg:py-16 xl:px-16">
-
+                   
+                              
                 <div className="mb-5 inline-flex w-fit items-center gap-2 rounded-full border border-rose-100 bg-white/80 px-3.5 py-2 text-xs font-semibold text-rose-500 shadow-sm">
 
                   <Sparkles className="h-3.5 w-3.5" />
