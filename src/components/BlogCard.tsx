2b52bac770b567c18f0144307ae438d41b875b1d
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-15 10:50:52 +0530

    Remove unused avatar import from BlogCard component
    Removed unused rocksyAvatar import from BlogCard.tsx

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -9,9 +9,6 @@ import { Link } from 'react-router-dom';
 
 import type { BlogPost } from '../lib/blogApi';
 import BlogImage from './BlogImage';
-import rocksyAvatar from '/images/rocksky-avatar.webp';
-
-
 interface BlogCardProps {
   post: BlogPost;
 }
