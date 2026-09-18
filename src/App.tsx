60351fe7171067a718291144c149f95e88819b36
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-18 20:22:15 +0530

    Add new routes for CosmicTarot and love-calculator

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,9 @@ export default function App() {
               path="/cosmic-tarot"
               element={<CosmicTarot />}
             />
-
+              <Route path="/tools/tarot" element={<CosmicTarot />} />
+              <Route path="/love-calculator" element={<CosmicTarot />} />
+              <Route path="/tools/love-calculator" element={<CosmicTarot />} />
 
             {/* =========================================
                 BLOG LISTING
