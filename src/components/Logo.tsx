41564a36c81957dc6988f870c1cedc6e7f56d491
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-18 13:56:46 +0530

    Update logo source to loveons-logo.svg

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 export default function Logo({ className = 'w-8 h-8', alt = 'Loveons logo' }: LogoProps) {
   return (
     <img
-      src="/images/logo.svg"
+      src="/images/loveons-logo.svg"
       alt={alt}
       className={`object-contain flex-shrink-0 ${className}`}
       loading="eager"
