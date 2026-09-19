1f33d41b47392942819996a29ccb03ed6b59d9a5
roshanmachhi45-eng <roshan.machhi.45@gmail.com>
2026-09-19 15:16:33 +0530

    Fix JSX structure in BlogDetail component

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -2014,13 +2014,19 @@ CHAT GAME
             <div className="pt-6">
               <iframe src="/cosmic-tarot" className="w-full h-[80vh] border-0 rounded-2xl" />
             </div>
-          </div>        
-       )}
-     </div>
-    </div>
-    </>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
+
+       
+     
+    
+    
+  
+
 
 
 
